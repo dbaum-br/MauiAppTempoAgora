578f2d4c27324d7882984b678a691c7c628a43de
daniel baum <dbaum_br@hotmail.com>
2026-03-19 17:02:41 -0300

    Melhorias implementadas

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MauiAppTempoAgora.Models;
 using MauiAppTempoAgora.Services;
+using System.Diagnostics;
 
 namespace MauiAppTempoAgora
 {
@@ -12,14 +13,14 @@ namespace MauiAppTempoAgora
             InitializeComponent();
         }
 
-		private async void Button_Clicked(object sender, EventArgs e)
-		{
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
+        {
             try
             {
-                if(!string.IsNullOrEmpty(txt_cidade.Text))
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
                 {
                     Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
-                    if (t!= null)
+                    if (t != null)
                     {
                         string dados_previsao = "";
 
@@ -32,12 +33,22 @@ namespace MauiAppTempoAgora
 
 
                         lbl_res.Text = dados_previsao;
+
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                                      $"type=map&location=coordinates&metricRain=default&metricTemp=default" +
+                                      $"&metricWind=default&zoom=10&overlay=wind&product=ecmwf&level=surface" +
+                                      $"&lat={t.lat.ToString().Replace(",",".")}&lon={t.lon.ToString().Replace(",",".")}";
+
+                        wv_mapa.Source = mapa;
+
+                        Debug.WriteLine(mapa);
                     }
                     else
                     {
                         lbl_res.Text = "Sem dados de previsão";
                     }
-                }else
+                }
+                else
                 {
                     lbl_res.Text = "Preencha a cidade";
                 }
@@ -46,7 +57,66 @@ namespace MauiAppTempoAgora
             {
                 await DisplayAlert("Ops", ex.Message, "OK");
             }
-            
+
+        }
+
+        private async void Button_Clicked_Localizacao(object sender, EventArgs e)
+        {
+            try
+            {
+                GeolocationRequest request =
+                    new GeolocationRequest(GeolocationAccuracy.Medium,
+                    TimeSpan.FromSeconds(10)
+                    );
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+
+                if (local != null)
+                {
+                    string local_disp = $"Latitude: {local.Latitude} \n" +
+                        $"Longitude: {local.Longitude} \n";
+                    lbl_coords.Text = local_disp;
+
+                    // Pega nome da cidade de acordo com as coordenadas
+                    GetCidade(local.Latitude, local.Longitude);
+                }
+                else
+                {
+                    lbl_coords.Text = "Nenhuma localização";
+                }
+            }
+            catch (FeatureNotSupportedException fnEx)
+            {
+                await DisplayAlert("Erro: Dispositivo não suporta", fnEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Erro: Localização desabilitada", fneEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Erro: Permissão da localização", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro: ", ex.Message, "OK");
+            }
+        }
+
+        private async void GetCidade(double lat, double lon)
+        {
+            try
+            {
+                IEnumerable<Placemark> places = await Geocoding.Default.GetPlacemarksAsync(lat, lon);
+                Placemark? place = places.FirstOrDefault();
+                if (place != null)
+                {
+                    txt_cidade.Text = place.Locality;
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro: obtenção do nome da cidade", ex.Message, "OK");
+            }
         }
     }
 
